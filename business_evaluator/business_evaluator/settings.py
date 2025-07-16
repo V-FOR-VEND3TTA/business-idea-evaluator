7945f4c25c2ecafc470eea8f7096b108b73536b9
George Hlongwane <georgehlongwane8@gmail.com>
2025-07-16 12:39:16 +0200

    Added the needed middleware and necessary settings for login

--- business_evaluator/business_evaluator/settings.py
+++ business_evaluator/business_evaluator/settings.py
@@ -122,3 +122,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+]
+
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGOUT_REDIRECT_URL = 'login'
